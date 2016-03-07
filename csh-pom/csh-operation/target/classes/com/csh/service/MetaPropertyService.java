47a351d57317696efbd33a8e6f37a6d31e92378b
unknown <Administrator@CNPC0190-CD.cnt01.local>
2016-03-07 23:26:11 +0800

    delete operation target file

--- csh-pom/csh-operation/target/classes/com/csh/service/MetaPropertyService.java
+++
@@ -1,8 +0,0 @@
-package com.csh.service;
-
-import com.csh.entity.MetaProperty;
-import com.csh.framework.service.BaseService;
-
-public interface MetaPropertyService extends BaseService<MetaProperty, Long> {
-
-}
