2999a909c331c4fbc47d690da6d0e57347418699
sujinxuan <sujinxuan@CNPC0308-CD.cnt01.local>
2016-03-14 14:30:49 +0800

    generate enterprise code

---
+++ csh-pom/csh-enterprise/src/main/java/com/csh/service/impl/VehicleBrandDetailServiceImpl.java
@@ -0,0 +1,34 @@
+package com.csh.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.csh.dao.VehicleBrandDetailDao;
+import com.csh.entity.VehicleBrandDetail;
+import com.csh.framework.service.impl.BaseServiceImpl;
+import com.csh.service.VehicleBrandDetailService;
+
+@Service ("vehicleBrandDetailServiceImpl")
+public class VehicleBrandDetailServiceImpl extends
+    BaseServiceImpl<VehicleBrandDetail, Long> implements VehicleBrandDetailService
+{
+
+  @Resource (name = "vehicleBrandDetailDaoImpl")
+  private VehicleBrandDetailDao vehicleBrandDetailDaoDao;
+
+  @Resource
+  public void setBaseDao (VehicleBrandDetailDao vehicleBrandDetailDao)
+  {
+    super.setBaseDao (vehicleBrandDetailDao);
+  }
+
+  @Override
+  public List<VehicleBrandDetail> findRoots (Integer count)
+  {
+
+    return vehicleBrandDetailDaoDao.findRoots (count);
+  }
+}
--- csh-pom/csh-enterprise/src/main/java/com/csh/service/impl/VehicleBrandServiceImpl.java
+++ csh-pom/csh-enterprise/src/main/java/com/csh/service/impl/VehicleBrandServiceImpl.java
@@ -1,34 +1,19 @@
-package com.csh.service.impl;
+package com.csh.service.impl; 
 
-import java.util.List;
+import javax.annotation.Resource; 
 
-import javax.annotation.Resource;
+import org.springframework.stereotype.Service; 
 
-import org.springframework.stereotype.Service;
-
-import com.csh.dao.VehicleBrandDetailDao;
-import com.csh.entity.VehicleBrandDetail;
+import com.csh.entity.VehicleBrand;
+import com.csh.dao.VehicleBrandDao;
+import com.csh.service.VehicleBrandService;
 import com.csh.framework.service.impl.BaseServiceImpl;
-import com.csh.service.VehicleBrandDetailService;
 
-@Service ("vehicleBrandDetailServiceImpl")
-public class VehicleBrandServiceImpl extends
-    BaseServiceImpl<VehicleBrandDetail, Long> implements VehicleBrandDetailService
-{
+@Service("vehicleBrandServiceImpl")
+public class VehicleBrandServiceImpl extends BaseServiceImpl<VehicleBrand,Long> implements VehicleBrandService {
 
-  @Resource (name = "vehicleBrandDetailDaoImpl")
-  private VehicleBrandDetailDao vehicleBrandDetailDaoDao;
-
-  @Resource
-  public void setBaseDao (VehicleBrandDetailDao vehicleBrandDetailDao)
-  {
-    super.setBaseDao (vehicleBrandDetailDao);
-  }
-
-  @Override
-  public List<VehicleBrandDetail> findRoots (Integer count)
-  {
-
-    return vehicleBrandDetailDaoDao.findRoots (count);
+      @Resource(name="vehicleBrandDaoImpl")
+      public void setBaseDao(VehicleBrandDao vehicleBrandDao) {
+         super.setBaseDao(vehicleBrandDao);
   }
 }
