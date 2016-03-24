5cb68dd5bc552067bfdc0d40dcf274b702c47c0b
sujinxuan <sujinxuan@CNPC0308-CD.cnt01.local>
2016-03-24 18:26:57 +0800

    update user reg interface

--- csh-pom/csh-interface/src/main/java/com/csh/controller/EndUserController.java
+++ csh-pom/csh-interface/src/main/java/com/csh/controller/EndUserController.java
@@ -375,6 +375,9 @@ public class EndUserController extends MobileBaseController {
       }
       response.setCode(CommonAttributes.SUCCESS);
       response.setDesc(regUser.getId().toString());
+      String token = TokenGenerator.generateToken();
+      endUserService.createEndUserToken(token, regUser.getId());
+      response.setToken(token);
       return response;
     } else {
       // 短信验证码验证
@@ -501,5 +504,40 @@ public class EndUserController extends MobileBaseController {
     response.setToken(newtoken);
     return response;
   }
+
+
+  // /**
+  // * 获取用户信息
+  // *
+  // * @param req
+  // * @return
+  // */
+  // @RequestMapping(value = "/getUserInfo", method = RequestMethod.POST)
+  // @UserValidCheck
+  // public @ResponseBody ResponseOne<Map<String, Object>> getUserInfo(@RequestBody BaseRequest req)
+  // {
+  // ResponseOne<Map<String, Object>> response = new ResponseOne<Map<String, Object>>();
+  // Long userId = req.getUserId();
+  // String token = req.getToken();
+  //
+  // // 验证登录token
+  // String userToken = endUserService.getEndUserToken(userId);
+  // if (!TokenGenerator.isValiableToken(token, userToken)) {
+  // response.setCode(CommonAttributes.FAIL_TOKEN_TIMEOUT);
+  // response.setDesc(Message.error("csh.user.token.timeout").getContent());
+  // return response;
+  // }
+  //
+  // EndUser endUser = endUserService.find(userId);
+  //
+  // String[] properties = {"id", "userName", "nickName", "photo", "signature", "defaultVehicle"};
+  // Map<String, Object> map = FieldFilterUtils.filterEntityMap(properties, endUser);
+  // response.setMsg(map);
+  //
+  // String newtoken = TokenGenerator.generateToken(req.getToken());
+  // endUserService.createEndUserToken(newtoken, userId);
+  // response.setToken(newtoken);
+  // return response;
+  // }
 
 }
--- csh-pom/csh-interface/src/main/java/com/csh/utils/test.java
+++ csh-pom/csh-interface/src/main/java/com/csh/utils/test.java
@@ -1,28 +1,23 @@
 package com.csh.utils;
 
-import java.security.KeyPair;
-
-import org.apache.commons.codec.digest.DigestUtils;
-
-import com.csh.beans.Setting;
-import com.csh.utils.KeyGenerator;
-import com.csh.utils.RSAHelper;
-import com.csh.utils.SettingUtils;
 
 public class test {
 
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		Setting setting = SettingUtils.get();
-		String serverPrivateKey = setting.getServerPrivateKey();
-		String serverPublicKey = setting.getServerPublicKey();
-		KeyPair keyPair = KeyGenerator.generateKeys();
-		System.out.println(RSAHelper.getKeyString(keyPair.getPublic()));
-		System.out.println(RSAHelper.getKeyString(keyPair.getPrivate()));
-		String pwd = KeyGenerator.encrypt("111111", RSAHelper.getPublicKey(serverPublicKey));
-		System.out.println(pwd);
-		System.out.println(KeyGenerator.decrypt(pwd, RSAHelper.getPrivateKey(serverPrivateKey)));
-		System.out.println(DigestUtils.md5Hex("111111"));
-	}
+  public static void main(String[] args) throws Exception {
+    // TODO Auto-generated method stub
+    // Setting setting = SettingUtils.get();
+    // String serverPrivateKey = setting.getServerPrivateKey();
+    // String serverPublicKey = setting.getServerPublicKey();
+    // KeyPair keyPair = KeyGenerator.generateKeys();
+    // System.out.println(RSAHelper.getKeyString(keyPair.getPublic()));
+    // System.out.println(RSAHelper.getKeyString(keyPair.getPrivate()));
+    // String pwd = KeyGenerator.encrypt("111111", RSAHelper.getPublicKey(serverPublicKey));
+    // System.out.println(pwd);
+    // System.out.println(KeyGenerator.decrypt(pwd, RSAHelper.getPrivateKey(serverPrivateKey)));
+    // System.out.println(DigestUtils.md5Hex("111111"));
+    // Date date1 = new Date();
+    // Date date2 = new Date();
+    // System.out.println(DateUtils.isSameDay(date1, date2));
+  }
 
 }
