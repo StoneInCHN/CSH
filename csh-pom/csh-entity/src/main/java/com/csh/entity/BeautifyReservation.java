e13fa765c67f864c6448571c9ea9ab705882a336
yohu <82370288@qq.com>
2016-04-13 17:33:31 +0800

    美容预约entity

---
+++ csh-pom/csh-entity/src/main/java/com/csh/entity/BeautifyReservation.java
@@ -0,0 +1,154 @@
+package com.csh.entity;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Index;
+import org.hibernate.search.annotations.Analyzer;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.FieldBridge;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.IndexedEmbedded;
+import org.hibernate.search.annotations.Store;
+import org.wltea.analyzer.lucene.IKAnalyzer;
+
+import com.csh.entity.base.BaseEntity;
+import com.csh.entity.commonenum.CommonEnum.ReservationInfoFrom;
+import com.csh.lucene.DateBridgeImpl;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * 维修预约
+ * 
+ */
+@Indexed(index="beautifyReservation")
+@Entity
+@Table (name = "csh_beautify_reservation")
+@SequenceGenerator (name = "sequenceGenerator", sequenceName = "csh_beautify_reservation_sequence")
+public class BeautifyReservation extends BaseEntity
+{
+
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * 预约人
+   */
+  private EndUser endUser;
+
+  /**
+   * 预约时间
+   */
+  private Date reservationDate;
+
+  /**
+   * 预约车牌号
+   */
+  private String plate;
+
+  /**
+   * 预约车型
+   */
+  private String vehicleBrand;
+  
+  /**
+   * 预约信息来源
+   */
+  private ReservationInfoFrom reservationInfoFrom;
+
+  /**
+   * 服务记录
+   */
+  private CarServiceRecord carServiceRecord;
+  private Long tenantID;
+
+  @ManyToOne
+  @JsonProperty
+  @IndexedEmbedded
+  public EndUser getEndUser ()
+  {
+    return endUser;
+  }
+
+  public void setEndUser (EndUser endUser)
+  {
+    this.endUser = endUser;
+  }
+
+  @JsonProperty
+  @Field(index = org.hibernate.search.annotations.Index.UN_TOKENIZED, store = Store.NO)
+  @FieldBridge(impl = DateBridgeImpl.class)
+  public Date getReservationDate ()
+  {
+    return reservationDate;
+  }
+
+  public void setReservationDate (Date reservationDate)
+  {
+    this.reservationDate = reservationDate;
+  }
+
+  @JsonProperty
+  @Field(index = org.hibernate.search.annotations.Index.TOKENIZED, store = Store.NO,analyzer=@Analyzer(impl=IKAnalyzer.class))
+  public String getPlate ()
+  {
+    return plate;
+  }
+
+  public void setPlate (String plate)
+  {
+    this.plate = plate;
+  }
+
+  @JsonProperty
+  public String getVehicleBrand ()
+  {
+    return vehicleBrand;
+  }
+
+  public void setVehicleBrand (String vehicleBrand)
+  {
+    this.vehicleBrand = vehicleBrand;
+  }
+
+  @Index (name = "beautifyReservation_tenantid")
+  @Field(index = org.hibernate.search.annotations.Index.UN_TOKENIZED, store = Store.NO)
+  public Long getTenantID ()
+  {
+    return tenantID;
+  }
+
+  public void setTenantID (Long tenantID)
+  {
+    this.tenantID = tenantID;
+  }
+  
+  @JsonProperty
+  @Field(index = org.hibernate.search.annotations.Index.UN_TOKENIZED, store = Store.NO)
+  public ReservationInfoFrom getReservationInfoFrom ()
+  {
+    return reservationInfoFrom;
+  }
+
+  public void setReservationInfoFrom (ReservationInfoFrom reservationInfoFrom)
+  {
+    this.reservationInfoFrom = reservationInfoFrom;
+  }
+
+  @OneToOne(cascade = CascadeType.PERSIST)
+  public CarServiceRecord getCarServiceRecord ()
+  {
+    return carServiceRecord;
+  }
+
+  public void setCarServiceRecord (CarServiceRecord carServiceRecord)
+  {
+    this.carServiceRecord = carServiceRecord;
+  }
+
+}
