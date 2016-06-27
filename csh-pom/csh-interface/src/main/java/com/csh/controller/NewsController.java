db2a42fa65086e319e8f6e6e6d43a128df5b3684
Andrea <464709367@qq.com>
2016-06-27 14:21:05 +0800

    interface:add news

--- csh-pom/csh-interface/src/main/java/com/csh/controller/NewsController.java
+++ csh-pom/csh-interface/src/main/java/com/csh/controller/NewsController.java
@@ -1,5 +1,8 @@
 package com.csh.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
@@ -8,12 +11,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.csh.aspect.UserValidCheck;
 import com.csh.beans.CommonAttributes;
+import com.csh.beans.Message;
 import com.csh.controller.base.MobileBaseController;
+import com.csh.entity.News;
+import com.csh.framework.ordering.Ordering.Direction;
+import com.csh.framework.paging.Page;
+import com.csh.framework.paging.Pageable;
+import com.csh.json.base.BaseRequest;
 import com.csh.json.base.BaseResponse;
+import com.csh.json.base.PageResponse;
+import com.csh.json.base.ResponseMultiple;
 import com.csh.json.request.NewsRequest;
 import com.csh.service.EndUserService;
 import com.csh.service.NewsService;
+import com.csh.utils.FieldFilterUtils;
+import com.csh.utils.TokenGenerator;
 
 
 /**
@@ -31,6 +45,57 @@ public class NewsController extends MobileBaseController {
 
   @Resource(name = "newsServiceImpl")
   private NewsService newsService;
+
+
+  /**
+   * 获取新闻列表
+   * 
+   * @param req
+   * @return
+   */
+  @RequestMapping(value = "/getNewsList", method = RequestMethod.POST)
+  @UserValidCheck
+  public @ResponseBody ResponseMultiple<Map<String, Object>> getMsgList(@RequestBody BaseRequest req) {
+    ResponseMultiple<Map<String, Object>> response = new ResponseMultiple<Map<String, Object>>();
+    Integer pageSize = req.getPageSize();
+    Integer pageNumber = req.getPageNumber();
+    String token = req.getToken();
+    Long userId = req.getUserId();
+    // 验证登录token
+    String userToken = endUserService.getEndUserToken(userId);
+    if (!TokenGenerator.isValiableToken(token, userToken)) {
+      response.setCode(CommonAttributes.FAIL_TOKEN_TIMEOUT);
+      response.setDesc(Message.error("csh.user.token.timeout").getContent());
+      return response;
+    }
+
+    Pageable pageable = new Pageable();
+    pageable.setPageNumber(pageNumber);
+    pageable.setPageSize(pageSize);
+    pageable.setOrderProperty("createDate");
+    pageable.setOrderDirection(Direction.desc);
+    Page<News> newsList = newsService.findPage(pageable);
+
+    PageResponse pageInfo = new PageResponse();
+    pageInfo.setPageNumber(pageNumber);
+    pageInfo.setPageSize(pageSize);
+    pageInfo.setTotal((int) newsList.getTotal());
+
+
+    String[] propertys = {"id", "title", "content"};
+
+    List<Map<String, Object>> result =
+        FieldFilterUtils.filterCollectionMap(propertys, newsList.getContent());
+
+    response.setMsg(result);
+    response.setPage(pageInfo);
+
+    String newtoken = TokenGenerator.generateToken(token);
+    endUserService.createEndUserToken(newtoken, userId);
+    response.setToken(newtoken);
+    response.setCode(CommonAttributes.SUCCESS);
+    return response;
+  }
 
   /**
    * 推送新闻
